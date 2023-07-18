000181f306e4967da3732c0bd821e29a29deb2fe
wisp <forworkandtravel@yandex.ru>
2023-07-18 19:05:09 +0300

    ssl

---
+++ nginx_example/gen_cert.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+mkdir ./ssl && \
+openssl req -x509 -nodes \
+        -days 365 \
+        -newkey rsa:2048 \
+        -keyout ./ssl/private.key \
+        -out ./ssl/public.pem
+
+# curl --insecure https://127.0.0.1:8443
